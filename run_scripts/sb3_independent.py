af945b7a95fa3956a213c50cf09c413ea9acbbc0
Shuqing Shi <shuqingshi@nmslap007078.client.kcl.ac.uk>
2024-01-24 17:03:53 +0000

    independent

--- run_scripts/sb3_independent.py
+++ run_scripts/sb3_independent.py
@@ -184,7 +184,7 @@ def main(args):
         net_arch=[features_dim],
     )
 
-    tensorboard_log = "./results/sb3/cleanup_ppo_independent"
+    tensorboard_log = "./results/{env_name}_ppo_independent"
 
     model = IndependentPPO(
         "CnnPolicy",
