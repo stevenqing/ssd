238f68de5fca62b347edbd9a2bafbcdeaedf65ce
Shuqing Shi <shuqingshi@nmslap007078.client.kcl.ac.uk>
2024-02-12 00:43:57 +0000

    add CB

--- run_scripts/sb3_train.py
+++ run_scripts/sb3_train.py
@@ -16,6 +16,7 @@ import numpy as np
 import random
 from social_dilemmas.envs.pettingzoo_env import parallel_env
 
+
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 def set_seed(seed: int = 42) -> None:
@@ -108,6 +109,69 @@ def parse_args():
     return args
 
 
+class ChannelAttention(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Conv2d(channel, channel // reduction, 1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channel // reduction, channel, 1, bias=False)
+        )
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc(self.avg_pool(x))
+        max_out = self.fc(self.max_pool(x))
+        out = avg_out + max_out
+        return x * self.sigmoid(out)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        self.conv = nn.Conv2d(2, 1, kernel_size, padding=kernel_size//2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        out = torch.concat([avg_out, max_out], dim=1)
+        out = self.conv(out)
+        return x * self.sigmoid(out) 
+    
+class CBAM(BaseFeaturesExtractor):
+    def __init__(self, 
+                 observation_space: gym.spaces.Box, 
+                 channel=18, 
+                 features_dim=128,
+                 view_len=7, 
+                 num_frames=6, 
+                 fcnet_hiddens=[1024, 128], 
+                 reduction=16, 
+                 kernel_size=7):
+        
+        super(CBAM, self).__init__(observation_space,features_dim)
+
+        self.ca = ChannelAttention(channel, reduction)
+        self.sa = SpatialAttention(kernel_size)
+
+        flat_out = num_frames * 3 * (view_len * 2 + 1) ** 2 # eliminate the padding?
+
+        self.fc1 = nn.Linear(in_features=flat_out, out_features=fcnet_hiddens[0])
+        self.fc2 = nn.Linear(in_features=fcnet_hiddens[0], out_features=fcnet_hiddens[1])
+
+    def forward(self, x):
+        x = x.permute(0, 3, 1, 2)
+        x = self.ca(x)
+        x = self.sa(x)
+        features = torch.flatten(F.relu(x), start_dim=1)
+        features = F.relu(self.fc1(features))
+        features = F.relu(self.fc2(features))
+        return features
+
+
+
 # Use this with lambda wrapper returning observations only
 class CustomCNN(BaseFeaturesExtractor):
     """
@@ -172,6 +236,7 @@ def main(args):
         128  # output layer of cnn extractor AND shared layer for policy and value functions
     )
     fcnet_hiddens = [1024, 128]  # Two hidden layers for cnn extractor
+    kernal_size = 7
     ent_coef = 0.001  # entropy coefficient in loss
     batch_size = rollout_len * num_envs // 2  # This is from the rllib baseline implementation
     lr = 0.0001
@@ -211,7 +276,7 @@ def main(args):
     args = wandb.config # for wandb sweep
 
     policy_kwargs = dict(
-        features_extractor_class=CustomCNN,
+        features_extractor_class=CBAM,
         features_extractor_kwargs=dict(
             features_dim=features_dim, num_frames=num_frames, fcnet_hiddens=fcnet_hiddens
         ),
