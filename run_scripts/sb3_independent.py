40ed2f2eec8dbf6864c38e62d886120492edcd2f
Shuqing Shi <shuqingshi@nmslap007078.client.kcl.ac.uk>
2024-03-04 16:01:05 +0000

    fix lbf

--- run_scripts/sb3_independent.py
+++ run_scripts/sb3_independent.py
@@ -287,7 +287,7 @@ def main(args):
                             entity=args.user_name, 
                             notes=socket.gethostname(),
                             name=str(env_name) +"_" + str(extractor) + "_111_hard_" + str(model),
-                            group=str(env_name) + "_cf_selfish_" + str(model)+ "_independent_" + str(args.seed)+ "_" + str(args.alpha) + "_inequity_averse_" + str(args.inequity_averse_reward) + "_collective_" + str(args.use_collective_reward),
+                            group=str(env_name) + "_cf_lbf_modified_" + str(model)+ "_independent_" + str(args.seed)+ "_" + str(args.alpha) + "_inequity_averse_" + str(args.inequity_averse_reward) + "_collective_" + str(args.use_collective_reward),
                             dir="./",
                             job_type="training",
                             reinit=True)
