13484d49607892bc7c0c22d6a65e73130cca4aed
Shuqing Shi <shuqingshi@nmslap007078.client.kcl.ac.uk>
2024-03-10 22:03:32 +0000

    fix

--- run_scripts/sb3_independent.py
+++ run_scripts/sb3_independent.py
@@ -294,8 +294,8 @@ def main(args):
                             project="SSD_pytorch",
                             entity=args.user_name, 
                             notes=socket.gethostname(),
-                            name=str(env_name) + "_" + str(extractor) + "_" + str(model),
-                            group=str(env_name) +"_hard_cf_modified_discrete_" + str(model)+ "_independent_" + str(args.seed)+ "_" + str(args.alpha),
+                            name=str(env_name) + "_" + str(extractor) + "_" + str(model) ,
+                            group=str(env_name) +"_hard_cf_modified_discrete_" + str(model)+ "_independent_" + str(args.seed)+ "_" + str(args.alpha) + "_add_spawn_prob_" + str(add_spawn_prob),
                             dir="./",
                             job_type="training",
                             reinit=True)
