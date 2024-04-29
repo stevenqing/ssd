fix causal

stable_baselines3/common/buffers.py
@@ -403,7 +403,7 @@ class RolloutBuffer(BaseBuffer):
         self.all_actions = np.zeros((self.buffer_size, self.n_envs, self.agent_number, self.action_dim), dtype=np.float32)
         self.all_rewards = np.zeros((self.buffer_size, self.n_envs, self.agent_number), dtype=np.float32)
         self.all_dones = np.zeros((self.buffer_size, self.n_envs, self.agent_number), dtype=np.float32)
-        self.cf_rewards = np.zeros((self.buffer_size, self.n_envs, self.agent_number), dtype=np.float32)
+        self.cf_rewards = np.zeros((self.buffer_size, self.n_envs, self.agent_number-1), dtype=np.float32)
         self.inequity_rewards = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
 
         self.all_last_obs_traj = []
@@ -526,7 +526,7 @@ class RolloutBuffer(BaseBuffer):
             else:
             # using cf
                 # delta = self.rewards[step] + alpha * np.sum(self.cf_rewards[step],axis=-1) + self.gamma * next_values * next_non_terminal - self.values[step]   
-                delta = self.rewards[step] + alpha * np.sum(np.sum(self.cf_rewards[step:step+16],axis=-1),axis=0) + self.gamma * next_values * next_non_terminal - self.values[step]   
+                delta = self.rewards[step] + alpha * np.sum(self.cf_rewards[step],axis=-1)/(self.agent_number - 1) + self.gamma * next_values * next_non_terminal - self.values[step]   
 
             last_gae_lam = delta + self.gamma * self.gae_lambda * next_non_terminal * last_gae_lam
             self.advantages[step] = last_gae_lam