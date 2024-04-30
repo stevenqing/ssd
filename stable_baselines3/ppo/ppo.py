b25446e43b9a1bc050c830a008c5f4d88c690912
Shuqing Shi <shuqingshi@nmslap007078.client.kcl.ac.uk>
2024-04-30 15:38:29 +0100

    fix

--- stable_baselines3/ppo/ppo.py
+++ stable_baselines3/ppo/ppo.py
@@ -804,8 +804,8 @@ class PPO(OnPolicyAlgorithm):
                     mask = (all_rewards == -1).any(dim=1)
                     selected_row_indices = mask.nonzero(as_tuple=False).squeeze()
                     cf_agents_rewards = th.cat((all_rewards[:, :self.polid], all_rewards[:, self.polid+1:]), dim=1)
-                    selfish_agent = th.where(cf_agents_rewards[selected_row_indices] == 1)[0]
-                    incentivate_reward = th.mean(rollout_data.cf_rewards[selected_row_indices, selfish_agent])
+                    selfish_agent = th.where(cf_agents_rewards[selected_row_indices] == 1)
+                    incentivate_reward = th.mean(rollout_data.cf_rewards[selected_row_indices][selfish_agent])
 
                     wandb.log({f"{self.polid}/selfish_incentivate_reward": incentivate_reward}, step=self.num_timesteps)
                     
