6fbcdad036c97c7924535017104909f69074b2d6
Shuqing Shi <shuqingshi@nmslap007078.client.kcl.ac.uk>
2024-05-18 16:00:14 +0100

    fix

--- exp_results/plot.py
+++ exp_results/plot.py
@@ -1,34 +1,29 @@
-# plot from downloaded data
 import os
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import numpy as np
+from plot_utils import exponential_moving_average
 
 root_dir = f"./data/"
 print(os.path.exists(root_dir))
 
 METHODs = ['Selfish', 'CF', 'SVO', 'Inequity']
-SCENARIOs = ['Coin', 'LBF', 'Cleanup', "Common_Harvest"]
-# for each env
+SCENARIOs = ['Coin', 'Level-Based Foraging', 'Cleanup', "Common_Harvest"]
 COLORs = ['r', 'hotpink', 'c', 'b', 'dodgerblue', 'mediumpurple',
           'cadetblue', 'steelblue', 'mediumslateblue', 'hotpink', 'mediumturquoise']
-
-COLORs = reversed(COLORs[:len(METHODs)])
-
+COLORs = list(reversed(COLORs[:len(METHODs)]))
 color_dict = {k: v for k, v in zip(METHODs, COLORs)}
-LINE_STYPLEs = ['solid' for i in range(20)]
+LINE_STYPLEs = ['solid' for _ in range(20)]
 pos_dict = {
     'Coin': 141,
-    'LBF': 142,
+    'Level-Based Foraging': 142,
     'Cleanup': 143,
     'Common_Harvest': 144,
 }
 
 # To Configure
 map_scenario_to_name = {k: k for k in SCENARIOs}
-map_scenario_to_name.update({})
-
 map_method_to_name = {k: k for k in METHODs}
-map_method_to_name.update({})
 
 rewards_list = []
 data_dict = {}
@@ -54,16 +49,12 @@ for scenario_tag in SCENARIOs:
             if scenario_tag == 'Coin':
                 data_dict[scenario_tag][method_name][seed] = rewards[:158]
             elif scenario_tag == 'Cleanup':
-                data_dict[scenario_tag][method_name][seed] = rewards[30:158]
-            elif scenario_tag == 'LBF':
+                data_dict[scenario_tag][method_name][seed] = rewards[39:158]
+            elif scenario_tag == 'Level-Based Foraging':
                 data_dict[scenario_tag][method_name][seed] = rewards[:158]
             else:
-                data_dict[scenario_tag][method_name][seed] = rewards[: 200]
+                data_dict[scenario_tag][method_name][seed] = rewards[39:158]
 
-import matplotlib.pyplot as plt
-import numpy as np
-from plot_utils import *
-# plt.style.use('fivethirtyeight')
 sorted_methods_list = METHODs
 
 
@@ -79,15 +70,12 @@ def draw_each(env_name, data_dict, i, color_list, map_method_to_name):
             print(f"Skip {env_name} {method}, since no data")
             continue
         max_length = np.array(len_list).max()
-        # print(method, data.keys(), max_length)
-
-        timestep = np.arange(max_length) * 6400  # / 100
+        timestep = np.arange(max_length) * 6400  # Adjusting the scale here
 
         reward_plot = np.zeros((seed_num, max_length))
         for meth_name, data_i in data.items():
             if len(data_i) == max_length:
                 reward_plot = np.array(list(data_i))[np.newaxis, :]
-                # reward_plot = np.nan_to_num(reward_plot)
                 reward_plot = reward_plot.repeat(seed_num, axis=0)
                 break
         lw = 2.5
@@ -97,53 +85,38 @@ def draw_each(env_name, data_dict, i, color_list, map_method_to_name):
             reward_plot[index, :len(data[run_name])] = data[run_name]
         reward = np.array(reward_plot)
         r_mean, r_std = np.mean(reward, axis=0), np.std(reward, axis=0, ddof=1)
-        # if config['min_value'] is not None:
-        # plt.ylim(config['min_value'], config['max_value'])
-        # plt.xlim(-1, config['max_step'])
-        # plt.ylim(-2., 0.5)
-        # else:
-        # plt.set_ylim(x.min() *2, x.max()*2)
 
         import matplotlib as mpl
         mpl.rcParams['axes.formatter.use_mathtext'] = True
         mpl.rcParams['mathtext.fontset'] = 'stix'
         mpl.rcParams['font.size'] = 20
 
-        plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
-        plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
         plt.xticks(fontsize=20)
         plt.yticks(fontsize=20)
 
-        # plt.set_yticklabels(plt.get_yticks(), fontsize=20)
-        # plt.set_xticklabels(plt.get_xticks(), fontsize=20)
-        # timestep = (timestep / 10000).i
-        plt.plot(timestep * 100, exponential_moving_average(r_mean, 1), color=color,
-                 label=map_method_to_name[method],  # +'-' + str(seed_num),
+        # Select range for "Cleanup" environment
+
+        plt.plot(timestep / 1e8, exponential_moving_average(r_mean, 1), color=color,
+                 label=map_method_to_name[method],
                  linewidth=lw, linestyle='solid',
                  )
-        # set yticks
-        # plt.xticks(np.arange(0, 80000, 10000))
-        # print(method, r_mean)
-        # r_std = r_std * 0.7
-        plt.fill_between(timestep * 100, exponential_moving_average(r_mean - r_std, 1), exponential_moving_average(r_mean + r_std, 0.1), alpha=0.1,
+        plt.fill_between(timestep / 1e8, exponential_moving_average(r_mean - r_std, 1), exponential_moving_average(r_mean + r_std, 0.1), alpha=0.1,
                          color=color)
-        # if 'causal' in config["method"]:
-    # plt.legend(loc="best", bbox_to_anchor=(1.0, 0.0), borderaxespad=0.1, borderpad=0.2, fontsize=7)
-    # plt.ylabel('Reward', fontsize=32)
 
     axes = plt.gca()
     axes.set_title(env_name, fontsize=32, y=1.07)
 
-    plt.xlabel('Number of Timesteps', fontsize=25, loc='center')  # $(×10^4)
+    plt.xlabel('Number of Timesteps (×1e8)', fontsize=25, loc='center')
     plt.grid()
 
-# plot each scenario as a subplot, and each method as a line
+    # Setting the x-axis scale to match 10^8
+    x_ticks = np.linspace(0, max(timestep) / 1e8, num=6)
+    x_ticks_labels = ['0', '0.2', '0.4', '0.6', '0.8', '1.0']
+    plt.xticks(ticks=x_ticks, labels=x_ticks_labels)
+
 
 plt.figure(figsize=(32, 6))
-# print([k for k, v in DATA.items()])
-# plt.text(0.06, 0.5, "y_label", va='center', rotation='vertical', fontsize=32)
 for scenario_tag, data_for_each_env in data_dict.items():
-    # print(env_name)
     if scenario_tag in pos_dict:
         i = pos_dict[scenario_tag]
     else:
@@ -154,18 +127,13 @@ for scenario_tag, data_for_each_env in data_dict.items():
     draw_each(map_scenario_to_name[scenario_tag],
               data_for_each_env, i, color_list=color_dict, map_method_to_name=map_method_to_name)
 
-
 fig = plt.gcf()
 ax = fig.get_axes()[0]
 handles, labels = ax.get_legend_handles_labels()
-# plt.legend(loc="best", bbox_to_anchor=(1.0, 0.0), borderaxespad=0.1, borderpad=0.2, fontsize=7)
 print(len(handles), len(labels))
 print(labels)
-# labels = [s.split('---5')[0] for s in labels]
-# labels = METHODs
-fig.text(0.085, 0.5, "Social Good",
+fig.text(0.085, 0.5, "Episodic Collective Reward",
          va='center', rotation='vertical', fontsize=32)
-# fig.text(0.5, 0.0, "Number of frames $(×10^4)$", ha='center', va='center', fontsize=32)
 
 legend = fig.legend(handles, labels, loc='lower center', ncol=7, fontsize=32,
                     bbox_to_anchor=(0.5, -0.3), bbox_transform=fig.transFigure)
@@ -173,7 +141,6 @@ legend = fig.legend(handles, labels, loc='lower center', ncol=7, fontsize=32,
 for line in legend.get_lines():
     line.set_linewidth(5)
 plt.subplots_adjust(hspace=0.45)
-
 
 plt.savefig("main_results.pdf", bbox_inches='tight')
 plt.show()
